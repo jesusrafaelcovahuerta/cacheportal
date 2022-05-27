53eb1c01bb568c2447af0065d9b39aeeec3ee4c3
emkrypted <jesuscova@jisparking.cl>
2022-05-27 11:54:05 -0400

    cambios

--- resources/js/components/Content.vue
+++ resources/js/components/Content.vue
@@ -191,7 +191,6 @@
     export default {
         created() {
             this.getRol();
-            this.storeAudit();
             this.getAlliaceList();
             this.getSectionList();
             this.getCategoryList();
