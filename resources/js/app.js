444b72b5623c0fd432e6cd7945eeec916a6d132b
emkrypted <jesuscova@jisparking.cl>
2022-05-27 13:15:06 -0400

    cambios

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,12 +8,17 @@ import VueAWN from "vue-awesome-notifications"
 import VueMask from 'v-mask';
 import vueVimeoPlayer from 'vue-vimeo-player';
 import excel from 'vue-excel-export'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.component(
     'menu-component',
     require('./components/Menu.vue').default
 );
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.use(excel)
 Vue.use(vueVimeoPlayer)
 Vue.use(VueAWN);
