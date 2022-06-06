37439a0ff4a486ec2d55798122d28d9bb211e0ea
emkrypted <jesuscova@jisparking.cl>
2022-06-06 07:05:37 -0400

    cambios

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -6,7 +6,7 @@
                     <font class="section_title">{{ post.section_title }}</font><br><i v-bind:class="post.icon"></i>
                 </router-link>
                 <button v-if="post.link_question_id == 1" class="boton2" :style="{ background: post.color}" v-on:click="goWeb(post.url)" >
-                    <font class="section_title">{{ post.section_title }}</font><br> <i v-bind:class="post.icon"></i>
+                    <font class="button_section_title">{{ post.section_title }}</font><br> <i v-bind:class="post.icon"></i>
                 </button>
 		    </div>
         </div>
