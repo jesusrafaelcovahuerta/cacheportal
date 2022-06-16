477ce9bdf86ea9a27887b1f82e19c56e1e883dda
emkrypted <jesuscova@jisparking.cl>
2022-06-16 19:34:24 -0400

    cambios

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -2,10 +2,10 @@
     <div class="container pt-32">
         <div class="row">
 		    <div class="col-6" v-for="(post, index) in posts" v-bind:index="index">
-                <router-link v-if="post.link_question_id == 2" class="boton2" :style="{ background: post.color}" :to="`/section/show/${post.section_id}`" >
+                <router-link @click="Track(post.google_tag)" v-if="post.link_question_id == 2" class="boton2" :style="{ background: post.color}" :to="`/section/show/${post.section_id}`" >
                     <font class="section_title">{{ post.section_title }}</font><br><i v-bind:class="post.icon"></i>
                 </router-link>
-                <button v-if="post.link_question_id == 1" class="boton2" :style="{ background: post.color}" v-on:click="goWeb(post.url)" >
+                <button v-if="post.link_question_id == 1" class="boton2" :style="{ background: post.color}" v-on:click="goWeb(post.url,post.google_tag)" >
                     <font class="section_title">{{ post.section_title }}</font><br> <i v-bind:class="post.icon"></i>
                 </button>
 		    </div>
@@ -21,13 +21,19 @@
             this.Track();
         },
         methods: {
-            Track () {
-                this.$gtag.pageview('/home');
+            Track(google_tag) {
+                this.$gtag.event('page_view', {
+                    page_title: google_tag
+                });
             },
             hideModal() {
                 this.modalShow = false;
             },
-            goWeb(url) {
+            goWeb(url, google_tag) {
+                this.$gtag.event('page_view', {
+                    page_title: google_tag
+                });
+
                 window.location.href = url;
             },
             checkDate() {
