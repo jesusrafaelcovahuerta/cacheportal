3c4f46dbec98dbdefee8bde85e8cab1ebad9421f
emkrypted <jesuscova@jisparking.cl>
2022-07-07 22:20:41 -0400

    cambios

--- resources/views/auth/layouts/app.blade.php
+++ resources/views/auth/layouts/app.blade.php
@@ -26,14 +26,7 @@
 <body class="bg-gradient-primary">
     @yield('content')
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('backend/vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('backend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="{{ asset('backend/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="{{ asset('backend/js/sb-admin-2.min.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
 
