fca9e65594ea8caecec5771bac2da0d928c84ca5
emkrypted <jesuscova@jisparking.cl>
2022-07-07 22:32:28 -0400

    cambios

--- resources/js/components/Menu.vue
+++ resources/js/components/Menu.vue
@@ -8,21 +8,21 @@
         </li>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item" v-if="rol_id == 1">
+        <li class="nav-item" v-if="rol_id == 1 || rol_id == 3">
             <router-link class="nav-link" to="/alliance">
             <i class="fas fa-fw fa-handshake"></i>
             <span>Alianzas</span></router-link>
         </li>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item" v-if="rol_id == 1">
+        <li class="nav-item" v-if="rol_id == 1 || rol_id == 3">
             <router-link class="nav-link" to="/section">
             <i class="fas fa-fw fa-arrow-up"></i>
             <span>Secciones</span></router-link>
         </li>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item" v-if="rol_id == 1">
+        <li class="nav-item" v-if="rol_id == 1 || rol_id == 3">
             <router-link class="nav-link" to="/category">
             <i class="fas fa-fw fa-signal"></i>
             <span>Categorias</span></router-link>
@@ -36,28 +36,28 @@
         </li>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item" v-if="rol_id == 1">
+        <li class="nav-item" v-if="rol_id == 1 || rol_id == 3">
             <router-link class="nav-link" to="/poll">
             <i class="fas fa-fw fa-poll"></i>
             <span>Encuestas</span></router-link>
         </li>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item" v-if="rol_id == 1">
+        <li class="nav-item" v-if="rol_id == 1 || rol_id == 3">
             <router-link class="nav-link" to="/metric">
             <i class="fas fa-fw fa-database"></i>
             <span>Metricas</span></router-link>
         </li>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item" v-if="rol_id == 1">
+        <li class="nav-item" v-if="rol_id == 1 || rol_id == 3">
             <router-link class="nav-link" to="/audit">
             <i class="fas fa-fw fa-thumbs-up"></i>
             <span>Auditoría</span></router-link>
         </li>
 
         <!-- Nav Item - Dashboard -->
-        <li class="nav-item" v-if="rol_id == 1">
+        <li class="nav-item" v-if="rol_id == 1 || rol_id == 3">
             <router-link class="nav-link" to="/user">
             <i class="fas fa-fw fa-users"></i>
             <span>Usuarios</span></router-link>
