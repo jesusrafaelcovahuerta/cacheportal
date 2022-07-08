3684213b3c0f0cd240346f8ff886c08a257117a1
emkrypted <jesuscova@jisparking.cl>
2022-07-07 22:24:36 -0400

    cambios

--- resources/views/auth/layouts/app.blade.php
+++ resources/views/auth/layouts/app.blade.php
@@ -27,13 +27,19 @@
     @yield('content')
     <!-- Bootstrap core JavaScript-->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="{{ asset('backend/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('backend/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('backend/js/sb-admin-2.min.js') }}"></script>
+
+    <script <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.3.4/jquery.inputmask.bundle.min.js"></script>></script>
 
     <script>
       $(document).ready(function(){
-        alert(1);
-        $('#rut').mask('00/00/0000');
+        $('#rut').inputmask();
       });
     </script>
 </body>
