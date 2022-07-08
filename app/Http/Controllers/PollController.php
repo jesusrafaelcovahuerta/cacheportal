8342b0b9d0e2bd14fe3d6b5cd3aa4bd67e30b1e1
emkrypted <jesuscova@jisparking.cl>
2022-07-08 08:27:27 -0400

    cambios

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -38,6 +38,16 @@ class PollController extends Controller
     }
 
     /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function result()
+    {
+        return view('backend.index');
+    }
+
+    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
