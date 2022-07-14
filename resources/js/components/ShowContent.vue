978fb168d8395e699b6efa1aa3571e38524e04c7
emkrypted <jesuscova@jisparking.cl>
2022-07-14 07:14:09 -0400

    cambios

--- resources/js/components/ShowContent.vue
+++ resources/js/components/ShowContent.vue
@@ -14,16 +14,21 @@
                     </center>
                 </div>
                 <div v-if="videoID !== null">
-                    <vimeo-player
-                        class="vimeo"
-                        ref="player"
-                        :options="{ responsive: true, autoplay: true }"
-                        :video-id="videoID"
-                        @ready="onReady"
-                        :autoplay="true"
-                        :player-height="320"
-                        :player-width="640"
-                    ></vimeo-player>
+                    <div v-if="this.post.video_type_id == 1">
+                        <iframe width="560" height="315" :src="`https://www.youtube.com/embed/${videoID}?autoplay=1`" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                    <div v-if="this.post.video_type_id == 2">
+                        <vimeo-player
+                            class="vimeo"
+                            ref="player"
+                            :options="{ responsive: true, autoplay: true }"
+                            :video-id="videoID"
+                            @ready="onReady"
+                            :autoplay="true"
+                            :player-height="320"
+                            :player-width="640"
+                        ></vimeo-player>
+                    </div>
                 </div>
                 <hr>	
                 <h3>{{ this.post.description }}</h3>	    
