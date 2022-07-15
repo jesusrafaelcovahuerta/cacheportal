3ae231479c1a0c9886a2d8e33768329584d578ad
emkrypted <jesuscova@jisparking.cl>
2022-07-15 10:41:21 -0400

    camnbios

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="container pt-32">
-        <b-alert show variant="secondary">Haz click en los botones para ver cada contenido</b-alert>
+        <div class="row">
+            <hr>
+            <h2><center><strong>Haz click en los botones para ver cada contenido</strong></center></h2>
+        </div>
         <div class="row">
 		    <div class="col-6" v-for="(post, index) in posts" v-bind:index="index">
                 <router-link @click.native="Track(post.google_tag)" v-if="post.link_question_id == 2" class="boton2" :style="{ background: post.color}" :to="`/section/show/${post.section_id}`" >
