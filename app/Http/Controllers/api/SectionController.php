13c5c4e8113564bc254cbbdecaff3bf940a0fe47
emkrypted <jesuscova@jisparking.cl>
2022-08-03 07:36:39 -0400

    cambios

--- app/Http/Controllers/api/SectionController.php
+++ app/Http/Controllers/api/SectionController.php
@@ -77,7 +77,7 @@ class SectionController extends ApiResponseController
 
         if($request->video_id != '') {
             $word = "/";
-            echo $string = $request->video_id;
+            $string = $request->video_id;
  
             if(strpos($string, $word) !== false){
                 echo 3;
