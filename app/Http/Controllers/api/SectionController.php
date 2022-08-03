47f1de0226dddee7703df57f067d5052cf446de5
emkrypted <jesuscova@jisparking.cl>
2022-08-03 07:30:45 -0400

    cambios

--- app/Http/Controllers/api/SectionController.php
+++ app/Http/Controllers/api/SectionController.php
@@ -79,11 +79,11 @@ class SectionController extends ApiResponseController
             $word = "/";
             $string = $request->video_id;
  
-            if(strpos($mystring, $word) !== false){
+            if(strpos($string, $word) !== false){
                 $word = "=";
                 $string = $request->video_id;
 
-                if(strpos($mystring, $word) !== false){
+                if(strpos($string, $word) !== false){
                     $video = explode("=", $request->video_id);
                     $video = explode("&", $video[1]);
                     $section->video_id = $video[0];
