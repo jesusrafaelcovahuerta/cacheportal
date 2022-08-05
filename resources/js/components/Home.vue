6a50fea71b5c562642b87371300e3785ff14d92d
emkrypted <jesuscova@jisparking.cl>
2022-08-05 11:25:07 -0400

    cambios

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -138,4 +138,17 @@ form.example::after {
   clear: both;
   display: table;
 }
+
+::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+  color: black;
+  opacity: 1; /* Firefox */
+}
+
+:-ms-input-placeholder { /* Internet Explorer 10-11 */
+  color: black;
+}
+
+::-ms-input-placeholder { /* Microsoft Edge */
+  color: black;
+}
 </style>
