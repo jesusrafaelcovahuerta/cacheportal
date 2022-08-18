184b1447d125be0fb325e99c237f1f48d303cf36
emkrypted <jesuscova@jisparking.cl>
2022-08-18 06:15:29 -0400

    cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,9 +21,6 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ mix('css/frontendapp.css') }}" rel="stylesheet">
-
 
 
 </head>
