6180fed1d6a1998ea46bdb047ac76db53cb00d9a
emkrypted <jesuscova@jisparking.cl>
2022-08-18 06:14:56 -0400

    cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,31 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+	<meta http-equiv="Cache-Control" content="no-cache" />
+	<meta http-equiv="Pragma" content="no-cache" />
+	<meta http-equiv="expires" content="Fri, 18 Jul 2014 1:00:00 GMT" />
+	<meta content='yes' name='apple-mobile-web-app-capable'/>
+	<meta content='yes' name='mobile-web-app-capable'/>
+
+    <title>{{ config('app.name', 'Portal de la Fundación Conecta Mayor') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ mix('css/frontendapp.css') }}" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
+
 
 </head>
 <body>
@@ -147,6 +172,14 @@
         @yield('content')
     </div>
     
+	<!-- end toolbar bottom -->
+    <script src="{{ asset('frontend/js/jquery-3.4.1.min.js') }}"></script>
+	<script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
+	<script src="{{ asset('frontend/js/fakeLoader.js') }}"></script>
+	<script src="{{ asset('frontend/js/swiper.min.js') }}"></script>
+	<script src="{{ asset('frontend/js/jquery.big-slide.js') }}"></script>
+	<script src="{{ asset('frontend/js/main.j') }}s"></script>
+	<script src="https://cdn.plyr.io/3.6.4/plyr.js"></script>
 
 </body>
 </html>
