ebbcf1fe57e0d0dfda49735392f0f1b6afd09c0e
emkrypted <jesuscova@jisparking.cl>
2022-09-23 01:44:28 -0300

    cambios

--- resources/js/components/Qr.vue
+++ resources/js/components/Qr.vue
@@ -5,37 +5,24 @@
   </template>
   
   <script lang="ts">
-    import { Component, Vue } from "vue-property-decorator";
-    import { BeforeInstallPromptEvent } from "vue-pwa-install";
-  
-    @Component({})
-    export default class App extends Vue {
-      deferredPrompt: BeforeInstallPromptEvent | void;
-  
-      promptInstall() {
-        // Show the prompt:
+    export default {
+    name: "App",
+    data() {
+        return {
+        deferredPrompt: null
+        };
+    },
+    created() {
+        window.addEventListener("beforeinstallprompt", (e) => {
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        this.deferredPrompt = e;
+        });
+    },
+    methods: {
+        async install() {
         this.deferredPrompt.prompt();
-  
-        // Wait for the user to respond to the prompt:
-        this.deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === "accepted") {
-            console.log("User accepted the install prompt");
-          } else {
-            console.log("User dismissed the install prompt");
-          }
-  
-          this.deferredPrompt = null;
-        });
-      }
-  
-      created() {
-        this.$on("canInstall", (event: BeforeInstallPromptEvent) => {
-          // Prevent Chrome 67 and earlier from automatically showing the prompt:
-          event.preventDefault();
-  
-          // Stash the event so it can be triggered later:
-          this.deferredPrompt = event;
-        });
-      }
+        }
     }
+    };
   </script>
