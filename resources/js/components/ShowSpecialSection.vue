c8b09363e51e9beef9e48a74914c0bbdd12a5204
emkrypted <jesuscova@jisparking.cl>
2022-10-13 20:38:29 -0300

    cambios

--- resources/js/components/ShowSpecialSection.vue
+++ resources/js/components/ShowSpecialSection.vue
@@ -5,26 +5,27 @@
             <hr>
             <qrcode-stream @decode="onDecode"></qrcode-stream>
 
-            <!-- toolbar bottom -->
-            <div class="toolbar">
-                <div class="container">
-                    <ul class="toolbar-bottom toolbar-wrap">
-                        <li class="toolbar-item pt-10">
-                            <button class="toolbar-link" @click="goBack">
-                                <i class="icon ion-ios-arrow-back"></i> Regresar
-                            </button>
-                        </li>
-                        <li class="toolbar-item">
-                            <button class="toolbar-link" @click="goHome">
-                                <i class="icon ion-ios-home"></i> Inicio
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+           
         </div>
         <div class="container pt-32" v-if="this.$route.params.id == 68">
             <h1><center>Noticias</center></h1>
+        </div>
+        <!-- toolbar bottom -->
+        <div class="toolbar">
+            <div class="container">
+                <ul class="toolbar-bottom toolbar-wrap">
+                    <li class="toolbar-item pt-10">
+                        <button class="toolbar-link" @click="goBack">
+                            <i class="icon ion-ios-arrow-back"></i> Regresar
+                        </button>
+                    </li>
+                    <li class="toolbar-item">
+                        <button class="toolbar-link" @click="goHome">
+                            <i class="icon ion-ios-home"></i> Inicio
+                        </button>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 	
