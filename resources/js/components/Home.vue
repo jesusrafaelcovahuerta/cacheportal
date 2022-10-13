5b88c8477e46faa3c2ea9cba3f263cb76a3662ac
emkrypted <jesuscova@jisparking.cl>
2022-10-13 20:19:36 -0300

    cambios

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -17,7 +17,7 @@
                         <font class="section_title">Leer Qr</font><br><i v-bind:class="'fa-solid fa-qrcode'"></i>
                     </router-link>
                 </div>
-                <div v-else>
+                <div v-if="post.section_id != 67 && post.section_id != 68">
                     <div v-if="post.link_question_id == 2">
                         <router-link @click.native="Track(post.google_tag)" v-if="post.direct_content_question_id == 0 || post.direct_content_question_id == null" class="boton2" :style="{ background: post.color}" :to="`/section/show/${post.section_id}`" >
                             <font class="section_title">{{ post.section_title }}</font><br><i v-bind:class="post.icon"></i>
