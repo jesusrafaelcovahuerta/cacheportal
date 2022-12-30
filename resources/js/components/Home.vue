3d356b8ebb295df936dfe92ccc3b0876621097ea
jisjesuscova <jesuscova@jisparking.com>
2022-12-30 12:14:51 -0300

    cambios

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -24,7 +24,7 @@
                         </router-link>
                     </div>
                     <div v-if="post.section_id == 69">
-                        <a @click.native="Track(post.google_tag)" class="boton2" :style="{ background: post.color}" :href="`tel:${'+00000'}`" >
+                        <a @click.native="Track(post.google_tag)" class="boton2" :style="{ background: post.color}" :href="`tel:${'+6003606000'}`" >
                             <font class="section_title">Salcobrand</font><br><center><img style="width: 50px;" :src="'/../frontend/images/salcobrand-seeklogo.com.png'" alt=""></center>
                         </a>
                     </div>
